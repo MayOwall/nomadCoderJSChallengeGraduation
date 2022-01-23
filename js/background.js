6eb38d4ae38abbf10349a46436212bbaa3093c56
MayOwall <97934878+MayOwall@users.noreply.github.com>
2022-01-24 01:07:01 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -4,6 +4,5 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
-console.log(bgImage.src);
 
 document.body.appendChild(bgImage);
