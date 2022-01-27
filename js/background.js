f5abf43306a784b7e6ea937e914fa827b06a7f97
MayOwall <97934878+MayOwall@users.noreply.github.com>
2022-01-27 23:24:07 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["아기오구_가을준비.jpeg", "아기오구_독서.jpg", "아기오구_모험.jpeg", "아기오구_캠핑.JPG"];
+const images = ["fall.jpeg", "book.jpg", "adventure.jpeg", "camping.JPG"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
