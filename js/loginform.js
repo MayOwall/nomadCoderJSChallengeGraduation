026fa7949da24cebfc4fdb53f27a4c55ae6244bc
MayOwall <97934878+MayOwall@users.noreply.github.com>
2022-01-27 23:03:16 +0900

    Update loginform.js

--- js/loginform.js
+++ js/loginform.js
@@ -1,6 +1,3 @@
-/*
-목표 : 아이디를 입력하고, login버튼을 누르면, 기존의 로그인박스는 사라지고, hello (아이디)가 나타나도록 하기.
-*/
 const CLASSNAME_HIDDEN = "hidden";
 const USERNAME_KEY = "username";
 
@@ -12,7 +9,7 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 
 function onloginHandler(event) {
-  //submit 기본동작 방지  
+  //submit 기본동작(새로고침) 방지  
   event.preventDefault();
   //loginForm 사라지게 만들기
   loginForm.classList.add(CLASSNAME_HIDDEN);
